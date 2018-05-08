5e7429f9758fcc131241a50ce326123dbccecd40
Jameson Nuss <jamesonnuss@yahoo.com>
2018-05-08 11:17:07 -0700

    Adding in breakpoints for the navigation. Navigation styles

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -1,42 +1,37 @@
 jQuery( document ).ready(function($) {
-    /* Make all External Links and PDF's open in a new Tab */
-    // var host = new RegExp('/' + window.location.host + '/');
-    // $('a').each(function() {
-	//     if ((!host.test(this.href) && this.href.slice(0, 1) != "/" && this.href.slice(0, 1) != "#" && this.href.slice(0, 1) != "?") || this.href.indexOf('.pdf') > 0) {
-	// 	    $(this).attr({'target': '_blank'});
-	//     }
-	// });
-	// $('.toggle').click(function(e) {
-	//   e.preventDefault();
-	//   var $this = $(this);
-	//   if ($this.next().hasClass('show')) {s
-	//       $this.next().removeClass('show');
-	//       $this.next().slideUp(350);
-	//   } else {
-	//       $this.parent().parent().find('li .inner').removeClass('show');
-	//       $this.parent().parent().find('li .inner').slideUp(350);
-	//       $this.next().toggleClass('show');
-	//       $this.next().slideToggle(350);
-	//   }
-	// });
-
-    jQuery('ul#menu-main-menu').superfish();
-    jQuery('ul.sf-menu').superfish('destroy');
-
-    // jQuery('ul#menu-main-menu-1').slicknav();
-    // jQuery('.slicknav_btn').appendTo('.mobile-menu-container');
-    // jQuery('.slicknav_btn').on('click', function(event) {
-    //     event.stopImmediatePropagation();
-    // })
-    // jQuery('.slicknav_menu').on('click', function(event) {
-    //     event.stopImmediatePropagation();
-    // })
-    // jQuery(document).on("mousedown, touchstart, click",function(event){
-    //     if(jQuery('.slicknav_btn').hasClass('slicknav_open')) {
-    //         jQuery('ul#menu-main-menu-1').slicknav('close');
-    //     }
-    // });
-});
-jQuery( window ).on( "load", function($) {
-
+    function mainMenu(){
+        if ($(window).innerWidth() > 752) {
+            $('ul#menu-main-menu').superfish();
+        } else {
+            $('ul#menu-main-menu').superfish('hide');
+            $('ul#menu-main-menu').find('li.sfHover').removeClass('sfHover');
+            $('ul#menu-main-menu').superfish('destroy');
+        }
+    }
+    $("button.menu-icon").on( "click", function() {
+        event.stopImmediatePropagation();
+        $(this).toggleClass('active-menu');
+        $('#menu-main-menu').slideToggle('medium', function() {
+            if ($(this).is(':visible'))
+                $(this).css('display','flex');
+        });
+    });
+    $('#menu-main-menu').on('click', function(event) {
+        event.stopImmediatePropagation();
+    });
+    $(document).on("mousedown, touchstart, click",function(event){
+        if($('button.menu-icon').hasClass('active-menu')) {
+            $('#menu-main-menu').slideToggle('medium');
+            $("button.menu-icon").toggleClass('active-menu');
+        }
+    });
+    mainMenu();
+	var resizeTimer;
+	jQuery(window).on('resize', function(e) {
+	    clearTimeout(resizeTimer);
+	    var windowsize = jQuery(window).width();
+	    resizeTimer = setTimeout(function() {
+	           mainMenu();
+	    }, 250);
+	});
 });
--- parts/nav-main-menu.php
+++ parts/nav-main-menu.php
@@ -4,14 +4,14 @@
  */
 ?>
 <div class="row main-menu">
-	<div class="large-3 medium-12 small-12 columns">
+	<div class="large-3 medium-3 small-12 columns logo-container">
 		<div class="top-bar-left">
 			<ul class="menu">
 				<li><a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/site-logo.png" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>"/></a></li>
 			</ul>
 		</div>
 	</div>
-	<div class="large-9 medium-12 small-12 columns navigation-container">
+	<div class="large-9 medium-9 small-12 columns navigation-container">
 		<div class="top-bar-right">
 			<div class="top-menu" itemscope itemtype="http://schema.org/LocalBusiness">
 				<h1 itemprop="name"><?php bloginfo('name'); ?></h1>
@@ -20,8 +20,10 @@
 					<a href="tel:+1-<?php the_field('phone_number','option'); ?>" title=""><?php the_field('phone_number','option'); ?></a>
 				</span>
 			</div>
+			<span data-responsive-toggle="menu-main-menu" class="mobile-menu-button">
+		    	<button class="menu-icon dark" type="button" data-toggle>MENU</button>
+		    </span>
 			<?php joints_top_nav(); ?>
 		</div>
-		<div class="mobile-menu-container"><!-- Mobile Menu Populated Here --></div>
 	</div>
 </div>
