0fe28ec444aa508ac3cdf78f4a0d6cb4318a1c90
Jameson Nuss <jamesonnuss@yahoo.com>
2018-08-25 10:30:37 -0700

    Workarounds for navigation bugs, this will be the last time I use Foundation Top Bar, too many issues with breakpoints and cross-browser support. Going forward navigation from scratch will be my approach.

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -1,18 +1,73 @@
 jQuery( document ).ready(function($) {
+
+    var OSName="Unknown OS";
+    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+    else if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+    else if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+    else if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+    var browser = function() {
+        // Return cached result if avalible, else get result then cache it.
+        if (browser.prototype._cachedResult)
+            return browser.prototype._cachedResult;
+
+        // Opera 8.0+
+        var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+        // Firefox 1.0+
+        var isFirefox = typeof InstallTrigger !== 'undefined';
+
+        // Safari 3.0+ "[object HTMLElementConstructor]"
+        var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+        // Internet Explorer 6-11
+        var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+        // Edge 20+
+        var isEdge = !isIE && !!window.StyleMedia;
+
+        // Chrome 1+
+        var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+        // Blink engine detection
+        var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+        return browser.prototype._cachedResult =
+            isOpera ? 'Opera' :
+            isFirefox ? 'Firefox' :
+            isSafari ? 'Safari' :
+            isChrome ? 'Chrome' :
+            isIE ? 'IE' :
+            isEdge ? 'Edge' :
+            isBlink ? 'Blink' :
+            "Don't know";
+    };
+
     $('.anchor-button').on( "click", function() {
         $('html, body').animate({
             scrollTop: $( $.attr(this, 'href') ).offset().top
         }, 800);
         return false;
     })
+
+    var menuWidth = 768;
+
     function mainMenu(){
-        if ($(window).innerWidth() > 752) {
+
+        if (OSName == 'Windows' && browser() == 'Firefox' || OSName == 'Windows' && browser() == 'Edge'){
+            menuWidth = 769;
+            $('body').addClass('windows-navigation');
+        }
+
+        if (window.innerWidth > menuWidth) {
             $('ul#menu-main-menu').superfish();
             $("button.menu-icon").removeClass('active-menu');
+            console.log('start superfish');
         } else {
             $('ul#menu-main-menu').superfish('hide');
             $('ul#menu-main-menu').find('li.sfHover').removeClass('sfHover');
             $('ul#menu-main-menu').superfish('destroy');
+            console.log('destroy superfish');
         }
     }
     $("button.menu-icon").on( "click", function(event) {
@@ -23,15 +78,11 @@ jQuery( document ).ready(function($) {
                 $(this).css('display','flex');
         });
     });
-    $('#menu-main-menu').on('click', function(event) {
-        event.stopImmediatePropagation();
+
+    $("li.menu-item-has-children").each(function() {
+         $(this).attr('data-is-click', 'true')
     });
-    $(document).on("mousedown, touchstart, click",function(event){
-        if($('button.menu-icon').hasClass('active-menu')) {
-            $('#menu-main-menu').slideToggle('medium');
-            $("button.menu-icon").toggleClass('active-menu');
-        }
-    });
+
     mainMenu();
 	var resizeTimer;
 	jQuery(window).on('resize', function(e) {
@@ -41,18 +92,18 @@ jQuery( document ).ready(function($) {
 	           mainMenu();
 	    }, 250);
 	});
-    function liveChat(){
-        var headerHeight = $('.content-header').height();
-        var navHeight = $('header.header').height();
-        var showPosition = headerHeight + navHeight;
-        var scrollPosition = window.pageYOffset;
-        if (scrollPosition >= navHeight) {
-            $('.live-chat').fadeIn();
-        } else {
-            $('.live-chat').fadeOut();
-        }
-    }
-    liveChat();
+    // function liveChat(){
+    //     var headerHeight = $('.content-header').height();
+    //     var navHeight = $('header.header').height();
+    //     var showPosition = headerHeight + navHeight;
+    //     var scrollPosition = window.pageYOffset;
+    //     if (scrollPosition >= navHeight) {
+    //         $('.live-chat').fadeIn();
+    //     } else {
+    //         $('.live-chat').fadeOut();
+    //     }
+    // }
+    // liveChat();
     $(window).scroll(function() {
         liveChat();
     });
